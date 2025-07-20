67e0a457a9f87f57124bc0860e97defd4caa9287
Joel Bursztyn <Joel3141@gmail.com>
2025-07-20 17:53:02 +0300

    Commit my hello world

---
+++ Main.java
@@ -0,0 +1,7 @@
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class Main {
+System.out.println("Hello, World!");
+    SynthScrollBarUI synthScrollBarUI = new SynthScrollBarUI();
+    System.out.println("SynthScrollBarUI instance created: " + synthScrollBarUI);
+}
